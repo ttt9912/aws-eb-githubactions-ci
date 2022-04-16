df6a84f73d25038583dd4d4e165fc7103c9f521b
ttt <tschachtli.th@gmail.com>
2022-04-16 23:30:53 +0200

    metadata endpoint

--- src/main/java/ch/ttt/awsebgithubactionsci/InstanceInfoRestController.java
+++ src/main/java/ch/ttt/awsebgithubactionsci/InstanceInfoRestController.java
@@ -1,6 +1,5 @@
 package ch.ttt.awsebgithubactionsci;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -8,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Map;
 
 /*
@@ -19,20 +23,27 @@ public class InstanceInfoRestController {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/info")
-    public String healthCheck() {
-        final String token = restTemplate.exchange(
-                "http://169.254.169.254/latest/api/token",
-                HttpMethod.PUT,
-                new HttpEntity<>(Map.of("X-aws-ec2-metadata-token-ttl-seconds", 21600)),
-                String.class
-        ).getBody();
+    public String healthCheck() throws IOException, InterruptedException {
+        // https://www.baeldung.com/java-9-http-client
+        var client = HttpClient.newHttpClient();
 
-        final String az = restTemplate.exchange(
-                "http://169.254.169.254/latest/meta-data/placement/availability-zone",
-                HttpMethod.GET,
-                new HttpEntity<>(Map.of("X-aws-ec2-metadata-token", token)),
-                String.class
-        ).getBody();
+        final String token = client.send(
+                HttpRequest.newBuilder(URI.create("http://169.254.169.254/latest/api/token"))
+                        .header("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+                        .PUT(HttpRequest.BodyPublishers.noBody())
+                        .build(),
+                        HttpResponse.BodyHandlers.ofString())
+                .body();
+
+        log.info("token {}",token);
+
+        final String az = client.send(
+                        HttpRequest.newBuilder(URI.create("http://169.254.169.254/latest/meta-data/placement/availability-zone"))
+                                .header("X-aws-ec2-metadata-token", token)
+                                .GET()
+                                .build(),
+                        HttpResponse.BodyHandlers.ofString())
+                .body();
 
         return String.format("AZ: %s", az);
     }
