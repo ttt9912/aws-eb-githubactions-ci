cc14b33fe6744653d89da2f0bd8e11422e3d0113
ttt <tschachtli.th@gmail.com>
2022-04-16 23:46:42 +0200

    metadata endpoint

--- src/main/java/ch/ttt/awsebgithubactionsci/InstanceInfoRestController.java
+++ src/main/java/ch/ttt/awsebgithubactionsci/InstanceInfoRestController.java
@@ -1,18 +1,15 @@
 package ch.ttt.awsebgithubactionsci;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Map;
+import java.util.List;
 
 /*
  * Delete if it causes problems
@@ -20,29 +17,39 @@ import java.util.Map;
 @Slf4j
 @RestController
 public class InstanceInfoRestController {
-    private final RestTemplate restTemplate = new RestTemplate();
+    // https://www.baeldung.com/java-9-http-client
+    private final HttpClient client = HttpClient.newHttpClient();
 
     @GetMapping("/info")
-    public String healthCheck() throws IOException, InterruptedException {
-        // https://www.baeldung.com/java-9-http-client
-        var client = HttpClient.newHttpClient();
+    public List<String> info() throws IOException, InterruptedException {
+        final String token = requestToken();
 
-        final String token = client.send(
-                HttpRequest.newBuilder(URI.create("http://169.254.169.254/latest/api/token"))
-                        .header("X-aws-ec2-metadata-token-ttl-seconds", "21600")
-                        .PUT(HttpRequest.BodyPublishers.noBody())
-                        .build(),
-                        HttpResponse.BodyHandlers.ofString())
-                .body();
+        final String hostname = get(token, "http://169.254.169.254/latest/meta-data/hostname");
+        final String az = get(token, "http://169.254.169.254/latest/meta-data/placement/availability-zone");
 
-        final String az = client.send(
-                        HttpRequest.newBuilder(URI.create("http://169.254.169.254/latest/meta-data/placement/availability-zone"))
+        return List.of(
+                String.format("Hostname: %s", hostname),
+                String.format("AZ: %s", az)
+        );
+    }
+
+    private String get(final String token, final String url) throws IOException, InterruptedException {
+        return client.send(
+                        HttpRequest.newBuilder(URI.create(url))
                                 .header("X-aws-ec2-metadata-token", token)
                                 .GET()
                                 .build(),
                         HttpResponse.BodyHandlers.ofString())
                 .body();
+    }
 
-        return String.format("AZ: %s", az);
+    private String requestToken() throws IOException, InterruptedException {
+        return client.send(
+                        HttpRequest.newBuilder(URI.create("http://169.254.169.254/latest/api/token"))
+                                .header("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+                                .PUT(HttpRequest.BodyPublishers.noBody())
+                                .build(),
+                        HttpResponse.BodyHandlers.ofString())
+                .body();
     }
 }
